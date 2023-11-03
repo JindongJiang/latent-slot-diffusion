77ddd57724a75d499fb5648e891f6d3a95bc2c46
Jindong Jiang <jdpshq@gmail.com>
2023-11-03 14:41:12 -0400

    Update image_sampling.sh

--- configs/coco/image_sampling.sh
+++ configs/coco/image_sampling.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0  python src/eval/eval_stable_lsd_generation.py \
---ckpt_path /path_to_your_logs/lsd/coco/stable_lsd/checkpoint-400000/ \
+--ckpt_path /path_to_your_logs/lsd/coco/stable_lsd/checkpoint-xxx/ \
 --output_dir /path_to_your_image_logs \
 --enable_xformers_memory_efficient_attention --mixed_precision fp16 --num_workers 4
